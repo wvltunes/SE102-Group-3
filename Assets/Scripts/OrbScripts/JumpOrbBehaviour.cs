d113c53fc1e2fe36a7b3210ff2c35ca320b38c9a
FlashGuardian <crisbabygamer@gmail.com>
2026-04-07 13:07:48 +0700

    Initial implementation of orbs

---
+++ Assets/Scripts/OrbScripts/JumpOrbBehaviour.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class JumpOrbBehaviour : MonoBehaviour
+{
+    [SerializeField] private int laneToJump = 1;
+    private bool isTriggered = false;
+    private bool isPlayerInZone = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+            playerController.JumpPlayer(laneToJump);
+            isTriggered = true;
+            isPlayerInZone = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isTriggered = false;
+            isPlayerInZone = false;
+        }
+    }
+    public int getLaneToJump () 
+    { 
+        return laneToJump; 
+    }
+    public void setLaneToJump (int laneToJump)
+    {
+        this.laneToJump = laneToJump;
+    }
+}
---
+++ Assets/Scripts/OrbScripts/JumpOrbSpawner.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using System.Collections.Generic;
+using System.Collections;
+
+public class JumpOrbSpawner : MonoBehaviour
+{
+
+    public GameObject orbToSpawnPrefab;
+    public bool randomSpawn; //not true random, use for testing
+    public float bpm = 120;
+    public Vector2 spawnPoint;
+    private Coroutine spawnCoroutine;
+    public int orbLaneMultiplierToSpawn;
+    private Vector2 lane1Spawn = new Vector2 (7, -2.9f);
+    private Vector2 lane2Spawn = new Vector2(7, -0.9f);
+    private Vector2 lane3Spawn = new Vector2(7, 1.1f);
+    private Vector2 lane4Spawn = new Vector2(7, 3.1f);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (randomSpawn || spawnPoint == null)
+        {
+            RestartSpawner();
+        }
+        else
+        {
+            Instantiate(orbToSpawnPrefab, spawnPoint, Quaternion.identity);
+        }
+    }
+
+    public void RestartSpawner()
+    {
+        // If a loop is already running, kill it first
+        if (spawnCoroutine != null)
+        {
+            StopCoroutine(spawnCoroutine);
+        }
+
+        // Start a fresh loop and save it to our reference        
+        spawnCoroutine = StartCoroutine(SpawnRoutine());
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            float secondsPerPad = 240f / bpm;
+            int laneToSpawn = Random.Range(1, 4);
+            GameObject newPad;
+            switch (laneToSpawn)
+            {
+                case 1:
+                    newPad = Instantiate(orbToSpawnPrefab, lane1Spawn, Quaternion.identity);
+                    break;
+                case 2:
+                    newPad = Instantiate(orbToSpawnPrefab, lane2Spawn, Quaternion.identity);
+                    break;
+                case 3:
+                    newPad = Instantiate(orbToSpawnPrefab, lane3Spawn, Quaternion.identity);
+                    break;
+                case 4:
+                    newPad = Instantiate(orbToSpawnPrefab, lane4Spawn, Quaternion.identity);
+                    break;
+                default:
+                    newPad = Instantiate(orbToSpawnPrefab, lane1Spawn, Quaternion.identity);
+                    break;
+            }
+            JumpOrbVariables padVariables = newPad.GetComponent<JumpOrbVariables>();
+            padVariables.setLaneMultiplier (Random.Range(1, 4));
+            
+            yield return new WaitForSeconds(secondsPerPad);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
