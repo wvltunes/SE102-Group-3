2657a77bf643ac53145fae1e385cae0d5f939500
Nguyen Thi Ngoc Huyen <hinduck3206@gmail.com>
2026-05-31 17:51:38 +0700

    Fix Parallax and add BackGround for level1

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,40 +1,25 @@
 using UnityEngine;
 
-public class Parallax : MonoBehaviour
+public class MaterialParallax : MonoBehaviour
 {
-    private Material material;
-    [SerializeField] private float parallaxFactor= 0.1f;
-    private float offset;
-    public float gameSpeed = 5f;
+    private Material mat;
+    private Vector2 offset;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float speed = 0.1f;
+
     void Start()
     {
-        Renderer renderer = GetComponent<Renderer>();
-        if (renderer != null)
+        mat = GetComponent<Renderer>().material;
+
+        if (mat == null)
         {
-            material = renderer.material;
-        }
-        else
-        {
-            Debug.LogError("Renderer component not found on " + gameObject.name);
+            Debug.LogError("No material found on Renderer!");
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (material != null)
-        {
-            ParalaxScroll();
-        }
+        offset.x += speed * Time.deltaTime;
+        mat.mainTextureOffset = offset;
     }
-    private void ParalaxScroll()
-    {
-        float Speed = gameSpeed * parallaxFactor;
-        offset += Speed *Time.deltaTime;
-        material.SetTextureOffset("_MainTex", Vector2.right*offset);
-    }
-
-
 }
